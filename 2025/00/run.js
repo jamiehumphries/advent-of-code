10c00d5c5217437ed9955426839bc05d39032b1d
Jamie Humphries <jamie.humphries@softwire.com>
2025-12-17 12:05:57 +0000

    Add input parsing to template file

--- 2025/00/run.js
+++ 2025/00/run.js
@@ -1,3 +1,11 @@
+import { readFileSync } from "fs";
+import { resolve } from "path";
+
+const input = readFileSync(resolve(import.meta.dirname, "input.txt"))
+  .toString()
+  .trim()
+  .split("\n");
+
 export function run() {
-  console.log("Merry Christmas, world!");
+  console.log(input[0]);
 }
